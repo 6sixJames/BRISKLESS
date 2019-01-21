fb6303f42e33e671f1157fab651c93f7e86a58d1
David Lindley <dave@davelindley.com>
2019-01-21 18:39:34 -0500

    Set Up and Structure changes

--- FirebaseManager.py
+++ FirebaseManager.py
@@ -5,8 +5,17 @@ from Phidget22.Devices.TemperatureSensor import *
 import time
 from datetime import datetime
 
-#used as the document id within the database.
-START_TIME = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+setup = input("do you want to set up? y/n")
+
+if setup.lower() == "y":
+    # used as the document id within the database.
+    USER_NAME = input("Enter Username: ")
+    SMOKER = input("What kind of smoker do you have? ")
+    MEAT = input("What cut of meat are you cooking? ")
+elif setup.lower() == "n":
+    USER_NAME = "GALLAGHER_NICK"
+    SMOKER = "Weber 22"
+    MEAT = "Brisket"
 
 
 def onAttachHandler(self):
@@ -18,6 +27,20 @@ def onAttachHandler(self):
     self.setTemperatureChangeTrigger(0.0)
 
 
+def create_new_cook():
+    """
+    Initializes a new document in the firebase store, adds 'Cook level' data - i.e. Meat and Smoker Info.
+    :return:
+    """
+    cook_doc = db.collection(USER_NAME).document()
+    print(f"Created new cook for user {USER_NAME} with cook id:{cook_doc.id}")
+
+    cook_doc = db.collection(USER_NAME).document(cook_doc.id)
+    cook_doc.set({"smoker": SMOKER, "meat": MEAT})
+
+    return cook_doc.id
+
+
 def log_to_firebase(self, temperature):
     """
     This function is used as a mixin for the Phidget class - i.e. it will be used inside that class at runtime.
@@ -27,7 +50,7 @@ def log_to_firebase(self, temperature):
     :return:
     """
 
-    doc_ref = db.collection(START_TIME).document()
+    doc_ref = db.collection(USER_NAME).document(cook_id).collection("data").document()
     print(f"temperature {datetime.now()}---> {temperature}")
     doc_ref.set({"timestamp": datetime.now(), "temp": temperature})
 
@@ -65,6 +88,9 @@ if __name__ == "__main__":
 
     # db variable is used to log to firebase, despite not being explicitly passed.
     db = connect_to_firebase("briskless_firebase_auth.json")
+
+    # set up doc id for cook
+    cook_id = create_new_cook()
 
     # set the log to firebase mixin to run on every temp change
     BrisklessPhidget.setOnTemperatureChangeHandler(log_to_firebase)
