12f83744d8a06472b153e6308f30126244cc31d2
6sixJames <43579498+6sixJames@users.noreply.github.com>
2019-01-21 18:09:54 -0600

    Update test.py
    added timecooking field

--- test.py
+++ test.py
@@ -1,24 +1,36 @@
 from FirebaseManager import connect_to_firebase
 import pandas as pd
+from datetime import datetime
+
+
+def add_time_cooking(x, start_time):
+    return (x-start_time).total_seconds()
+
 
 db = connect_to_firebase("briskless_firebase_auth.json")
-
 
 user_id = u'GALLAGHER_NICK'
 
 docs = db.collection(user_id).get()
 
 dfs = []
+
+docs = db.collection(user_id).get()
+
+
 for doc in docs:
-    data = db.collection(user_id).document(doc.id).collection('data').get()
-    data_array = [{'temp': entry._data['temp'], 'time': entry._data['time']} for entry in data]
+    data = db.collection(user_id).document(doc.id).collection('data').order_by(u'timestamp').get()
+    data_array = [{'temp': entry._data['temp'], 'timestamp': entry._data['timestamp']} for entry in data]
 
     temp_df = pd.DataFrame(data_array)
     temp_df['user'] = user_id
     temp_df['grill_type'] = doc.to_dict()['smoker']
-    temp_df['meat_type'] = doc.to_dict()['Meat']
+    temp_df['meat_type'] = doc.to_dict()['meat']
+
+    start_time = temp_df['timestamp'].iloc[0]
+    temp_df['time_cooking'] = temp_df['timestamp'].apply(lambda x: add_time_cooking(x, start_time))
 
     dfs.append(temp_df)
 
 df = pd.concat(dfs)
-print(df)
+df.to_csv('test.csv')
