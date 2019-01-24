7b2955648df30be1dd8b5beecf197786b3f95eec
David Lindley <dave@davelindley.com>
2019-01-23 19:29:46 -0500

    Refactoring and Documentation.

--- FirebaseManager.py
+++ FirebaseManager.py
@@ -6,7 +6,19 @@ import time
 from datetime import datetime
 
 
+def ms_since_epoch():
+    """
+    This is used to calculate seconds in ms since 1.1.1970, which is the way HighCharts expects to recieve datetime.
+    :return: Milliseconds since January 1, 1970.
+    """
+    return int((datetime.utcnow() - datetime(1970, 1, 1)).total_seconds() * 1000)
+
+
 def get_custom_notes():
+    """
+    Loop for asking for user to enter additional metrics they would like to track.
+    :return: List of [{key:data},....]
+    """
     notes = []
     while input("additional notes? y/n").lower() == "y":
         key = input("What do you want to note")
@@ -18,13 +30,18 @@ def get_custom_notes():
 def onAttachHandler(self):
     """
     This needs to be done as a 'monkey patched' event handler - not sure why.
+    this gets passed into the Phidget class, and is treated as Phidget.onAttachHandler at runtime.
     Some settings can be changed here
     """
-    self.setDataInterval(1000)
+    self.setDataInterval(5000)
     self.setTemperatureChangeTrigger(0.0)
 
 
 def create_new_cook():
+    """
+    Initializes firebase document under USER_NAME collection.
+    :return: The ID of initialized firebase doc
+    """
     cook_doc = db.collection(USER_NAME).document()
     print(f"Created new cook for user {USER_NAME} with cook id:{cook_doc.id}")
 
@@ -47,11 +64,11 @@ def log_to_firebase(self, temperature):
     """
 
     doc_ref = db.collection(USER_NAME).document(cook_id).collection("data").document()
-    print(f"temperature {datetime.now()}---> {temperature}")
-    doc_ref.set({"timestamp": datetime.now(), "temp": temperature})
+    print(f"temperature {ms_since_epoch()}---> {temperature}")
+    doc_ref.set({"name": ms_since_epoch(), "y": temperature, "x": ms_since_epoch()})
 
 
-def configure_phidget(serial=542_616, hub=0, channel=0):
+def phidget_thermocouple_launch(serial=542_616, hub=0, channel=0):
     """
 
     :param serial: Serial Number as int
@@ -59,12 +76,12 @@ def configure_phidget(serial=542_616, hub=0, channel=0):
     :param channel: Channel 0 is Thermocouple. 1 is Board.
     :return: configured phidget
     """
-    ch = TemperatureSensor()
-    ch.setDeviceSerialNumber(serial)
-    ch.setHubPort(hub)
-    ch.setChannel(channel)
-    ch.setOnAttachHandler(onAttachHandler)
-    return ch
+    PhidgetTempSensor = TemperatureSensor()
+    PhidgetTempSensor.setDeviceSerialNumber(serial)
+    PhidgetTempSensor.setHubPort(hub)
+    PhidgetTempSensor.setChannel(channel)
+    PhidgetTempSensor.setOnAttachHandler(onAttachHandler)
+    return PhidgetTempSensor
 
 
 def connect_to_firebase(path_to_json_creds):
@@ -80,6 +97,7 @@ def connect_to_firebase(path_to_json_creds):
 
 
 if __name__ == "__main__":
+    # Boot loop example - runs set up and begins logging to firebase.
 
     setup = input("do you want to set up? y/n")
 
@@ -94,20 +112,22 @@ if __name__ == "__main__":
         USER_NAME = "GALLAGHER_NICK"
         SMOKER = "Weber 22"
         MEAT = "Brisket"
+        NOTES = []
 
-    BrisklessPhidget = configure_phidget()
+    BrisklessPhidget = phidget_thermocouple_launch()
 
     # db variable is used to log to firebase, despite not being explicitly passed.
+    # handled as variable within scope as work around for passing into log_to_firebase which is a monkey patched method.
     db = connect_to_firebase("briskless_firebase_auth.json")
 
     # set up doc id for cook
     cook_id = create_new_cook()
-
+    print("cook created")
     # set the log to firebase mixin to run on every temp change
     BrisklessPhidget.setOnTemperatureChangeHandler(log_to_firebase)
-
+    print("logging added")
     # launch temperature reading from Phidget
-    BrisklessPhidget.openWaitForAttachment(5000)
+    BrisklessPhidget.openWaitForAttachment(20000)
 
     while True:
         time.sleep(10)
